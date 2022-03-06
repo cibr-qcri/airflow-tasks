43a4a018abdf8e5b72abb3275198d2483ca8cf55
Isuranga Perera <isurangamperera@gmail.com>
2022-03-06 10:58:12 +0300

    Update policy

--- dags/darkweb-dag.py
+++ dags/darkweb-dag.py
@@ -4,14 +4,14 @@ from airflow import models
 from airflow.providers.cncf.kubernetes.operators.kubernetes_pod import KubernetesPodOperator
 
 
-YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
+YESTERDAY = datetime.datetime.now() + datetime.timedelta(days=1)
 
 with models.DAG(
         dag_id='darkweb-hourly-dags',
         schedule_interval='@hourly',
         start_date=YESTERDAY) as dag:
 
-    k = KubernetesPodOperator(
+    healthcheck_feed = KubernetesPodOperator(
         name="tor-healthcheck-domain-feed",
         image='toshiqcri/job-001:latest',
         image_pull_policy='Always',
