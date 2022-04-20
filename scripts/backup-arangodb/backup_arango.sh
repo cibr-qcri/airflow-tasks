5da8e8289e441d8ef7b231711480a4daa52938fd
Isuranga Perera <isurangamperera@gmail.com>
2022-04-20 10:31:03 +0300

    Add Airflow backup tasks for GB and ArangoDB

--- scripts/backup-arangodb/backup_arango.sh
+++ scripts/backup-arangodb/backup_arango.sh
@@ -1,5 +1,10 @@
-arangodump \
-  --server.endpoint "tcp://$ARANGO_HOST:$ARANGO_PORT" \
-  --server.username "$ARANGO_USERNAME" \
-  --all-databases true \
-  --output-directory "/backups/arangodb/"
+#arangodump \
+#  --server.endpoint "tcp://$ARANGO_HOST:$ARANGO_PORT" \
+#  --server.username "$ARANGO_USERNAME" \
+#  --all-databases true \
+#  --output-directory "/backups/arangodb/"
+
+while :
+do
+	sleep 1
+done
