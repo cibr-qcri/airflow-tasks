643958519d7234995621d2a43042b08ab4ac640e
sajithaliyanage <sajithaliyanage@gmail.com>
2022-06-07 14:49:20 +0300

    update scripts

--- scripts/eth-parser/dependencies/eth_blockchain_schema.sql
+++ scripts/eth-parser/dependencies/eth_blockchain_schema.sql
@@ -31,6 +31,7 @@ CREATE TABLE eth_transaction (
     nonce bigint,
     block_hash varchar(256),
     block_number bigint,
+    index bigint,
     transaction_index bigint,
     from_address varchar(256),
     to_address varchar(256),
