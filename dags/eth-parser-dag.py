dabd3e35e36fb8278f871fd4a17848fc8f2118b9
sajithaliyanage <sajithaliyanage@gmail.com>
2022-06-07 12:58:51 +0300

    update scripts

--- dags/eth-parser-dag.py
+++ dags/eth-parser-dag.py
@@ -9,7 +9,7 @@ YESTERDAY = datetime.datetime.now() - datetime.timedelta(days=1)
 
 volume_mount = VolumeMount(
     'cibr-airflow-pvc',
-    mount_path='/eth-blockchain-parser',
+    mount_path='/',
     sub_path=None,
     read_only=False
 )
